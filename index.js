581d7126e8a941ec6c3988fd5b0ec3e7095b977a
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-09-08 14:25:46 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const user=require("./user");
 http.createServer((req,res)=>{
     res.write("<h1>welcome to nodejs</h1>");
     res.write(user.UserName());
-    res.write("<br/>");
+    res.write("</br>");
     res.write(user.UserAge());
     res.end();
 }).listen(3001,()=>{
