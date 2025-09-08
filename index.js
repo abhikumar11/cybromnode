2705c387cd91d7acc34dd8ed83ed1eef14015db2
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-09-08 13:50:37 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ http.createServer((req,res)=>{
     res.write("<h1>welcome to nodejs</h1>");
     res.end();
 }).listen(3001,()=>{
-    console.log("Server is runing on port 3001");
-});
+    console.log("server is running on port 3001")
+})
