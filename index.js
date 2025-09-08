570f54543a6e1962914c6e6ef6afa6e116b05e7f
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-09-08 14:12:25 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const http=require("http");
-
+const user=require("./user");
 http.createServer((req,res)=>{
     res.write("<h1>welcome to nodejs</h1>");
+    res.write(user.UserName());
+    res.write("<br/>");
+    res.write(user.UserAge());
     res.end();
 }).listen(3001,()=>{
     console.log("server is running on port 3001")
---
+++ user.js
@@ -0,0 +1,10 @@
+const UserName=()=>{
+    return "My name is Abhishek";
+}
+const UserAge=()=>{
+    return "My age is 25";
+}
+
+module.exports={
+    UserName,UserAge
+}
