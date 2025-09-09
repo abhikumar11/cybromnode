6eece25b9262041f37cb819ed9740d8c98de7f4c
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-09-09 14:14:13 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const http=require("http");
-const user=require("./user");
+const uc=require("uppercase");
 http.createServer((req,res)=>{
     res.write("<h1>welcome to nodejs</h1>");
-    res.write(`<h2>${user.myDate()}</h2>`);
-    res.write(`<h2>${user.myCity()}</h2>`);
-    res.end("server response end");
+   res.write(uc("welCome tO tHe citY oF lAkEs"))
+    res.end("</br>server response end");
 }).listen(3001,()=>{
     console.log("server is running on port 3001")
 })
