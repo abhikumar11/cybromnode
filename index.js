c9b13d901ff2090cb1d426c408334ad62b151cb1
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-09-11 13:58:15 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const http=require("http");
-const uc=require("uppercase");
 const fs=require("fs");
-http.createServer((req,res)=>{
-    res.write("<h1>welcome to nodejs</h1>");   
-       res.write("welCome tO tHe citY oF lAkEs</br>")     
-   res.write(uc("welCome tO tHe citY oF lAkEs"))      
-  fs.readFile("doc.txt",(err,data)=>{
-        if(err){
-            throw err;    
-        }
-        res.write(`<p>${data}</p>`)
-        res.end("</br>server response end");
-   });
-    
-}).listen(3001,()=>{
-    console.log("server is running on port 3001")
+
+fs.appendFile("doc2.txt","hello i am abhishek from delhi",(err)=>{
+    if(err){
+        throw err;
+    }
+    console.log("new file created!!!");
 })
