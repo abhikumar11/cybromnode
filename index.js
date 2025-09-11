01519ee25ffccb77dfc0decd3c56f98c141ba837
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-09-11 14:15:22 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs=require("fs");
 
-fs.open("doc3.txt","w",(err,file)=>{
-    if(err)throw err;
+fs.writeFile("doc4.txt","we learn reactjs",(err)=>{
+    if(err) throw err;
     console.log("file created!!!");
 })
