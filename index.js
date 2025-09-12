5f2904040032ed2881022fd0b158a6d110f27889
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-09-12 13:50:15 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const bsname=path.basename("/users/document/index.js");
 console.log(bsname);
 const filename=path.basename("/users/document/index.js",".js");
 console.log(filename);
+
+console.log(__dirname);
+console.log(__filename);
