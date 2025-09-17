f56b1f9e46420a2030c5b6d906823c2e3be33811
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-09-17 14:06:46 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express=require("express");
 const app=express();
-const stuRoute=require("./routes/studentRoute");
-const employeeRoute=require("./routes/employeeRoute");
 const teacherRoute=require("./routes/teacherRoute");
 
-app.use("/students",stuRoute);
-app.use("/employee",employeeRoute);
 app.use("/teacher",teacherRoute);
 app.listen(3000,()=>{
     console.log("Server running on port 3000");
--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -1,18 +1,10 @@
 const express =require("express");
+const teacherController=require("../controller/teacherController");
 const router=express.Router();
 
-router.get("/home",(req,res)=>{
-    res.send("<h1>Welcome to teacher home page</h1>")
-})
-
-router.get("/about",(req,res)=>{
-    res.send("<h1>Welcome to teacher about page</h1>")
-})
-router.get("/subject",(req,res)=>{
-    res.send("<h1>Welcome to teacher subject</h1>")
-})
-router.get("/salary",(req,res)=>{
-    res.send("<h1>Welcome to teacher salary page</h1>")
-})
+router.get("/home",teacherController.homePage);
+router.get("/about",teacherController.aboutPage);
+router.get("/subject",teacherController.subjectPage);
+router.get("/salary",teacherController.salaryPage);
 
 module.exports=router;
