e0b3d717787cd2b371faf127dcf5f618b61a09c6
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-10-07 08:21:34 +0530

    initial commit

--- server/controllers/StudentController.js
+++ server/controllers/StudentController.js
@@ -1,8 +1,10 @@
-const Student=require("../models/StudentModel");
+const Student = require("../models/StudentModel");
+
 
 const createStudent=async(req,res)=>{
     const {rollno,name,city,fees}=req.body;
-    
+    const stu=new Student({rollno,name,city,fees});
+    await stu.save();
 }
 
 
