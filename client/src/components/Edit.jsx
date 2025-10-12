95ac7710cf98bf2c1e19001f116c8705868bc834
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-10-12 08:33:22 +0530

    initial commit

--- client/src/components/Edit.jsx
+++ client/src/components/Edit.jsx
@@ -26,13 +26,13 @@ const Edit = () => {
     <div>
         <h1>Edit Student Data</h1>
         <hr />
-         Enter Roll No:<input type="text" name="rollno" onChange={handleInput}/>
+         Enter Roll No:<input type="text" name="rollno" value={student.rollno} onChange={handleInput}/>
         <br/>
-        Enter Name:<input type="text" name="name" onChange={handleInput}/>
+        Enter Name:<input type="text" name="name" value={student.name} onChange={handleInput}/>
         <br/>
-        Enter City:<input type="text" name="city" onChange={handleInput}/>
+        Enter City:<input type="text" name="city" value={student.city} onChange={handleInput}/>
         <br/>
-        Enter Fees:<input type="text" name="fees" onChange={handleInput}/>
+        Enter Fees:<input type="text" name="fees" value={student.fees} onChange={handleInput}/>
         <br/>
         <button onClick={handleUpdate}>Update</button>
     </div>
