ccc95cf6aeb42b0d74d28d1d2de56984e193e454
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-10-14 13:59:22 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors=require("cors");
+require("dotenv").config();
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const stuRoute=require("./routes/StudentRoute");
@@ -9,10 +10,10 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/student",stuRoute);
-mongoose.connect("mongodb+srv://abhi:abhi@cluster1.p6lnqsp.mongodb.net/?retryWrites=true&w=majority&appName=cluster1").then(() => console.log("connected to db"));
+mongoose.connect(process.env.DBCONN).then(() => console.log("connected to db"));
 
 
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001")
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
 })
