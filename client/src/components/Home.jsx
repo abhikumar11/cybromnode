412dd63e4a2d346a1823315548b04b42f6a87c59
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-10-27 14:31:35 +0530

    initial commit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,41 @@
 import React from "react";
-
+import axios from "axios";
 const Home = () => {
+  const handleSubmit=async()=>{
+    const res=await axios.get(`${import.meta.env.VITE_SERVER}/home`);
+    console.log(res.data);
+  }
+    const handleAbout=async()=>{
+    const res=await axios.get(`${import.meta.env.VITE_SERVER}/about`);
+    console.log(res.data);
+  }
+    const handleService=async()=>{
+    const res=await axios.get(`${import.meta.env.VITE_SERVER}/service`);
+    console.log(res.data);
+  }
+   const btn1=async()=>{
+     const res=await axios.get(`${import.meta.env.VITE_SERVER}/home`);
+    console.log(res.data);
+   }
+    const btn2=async()=>{
+     const res=await axios.get(`${import.meta.env.VITE_SERVER}/about`);
+    console.log(res.data);
+   }
+    const btn3=async()=>{
+     const res=await axios.get(`${import.meta.env.VITE_SERVER}/service`);
+    console.log(res.data);
+   }
   return (
     <div>
       <h1>Welcome to Home Page</h1>
+      <button onClick={handleSubmit}>Home page</button>
+      <button onClick={handleAbout}>About page</button>
+      <button onClick={handleService}>Service page</button>
+          <br /><br />
+
+         <button onClick={btn1}>Btn1</button>
+      <button onClick={btn2}>Btn2</button>
+      <button onClick={btn3}>Btn3</button>
       <hr />
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -9,7 +9,31 @@ const stuRoute=require("./routes/StudentRoute");
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/student",stuRoute);
+//app.use("/student",stuRoute);
+
+app.use((req,res,next)=>{
+    console.log("First app level middleware");
+
+    next();
+})
+app.get("/home",(req,res)=>{
+    console.log("Home page");
+    res.send("Welcome to home page")
+})
+app.get("/about",(req,res)=>{
+    console.log("Route level middleware");
+    res.send("Welcome to about page")
+})
+app.get("/service",(req,res,next)=>{
+    console.log("Service page middleware");
+    next();
+},(req,res,next)=>{
+res.send("Welcome to service page")
+next();
+})
+app.use((req,res,next)=>{
+    console.log("last level middleware")
+})
 mongoose.connect(process.env.DBCONN).then(() => console.log("connected to db"));
 
 
