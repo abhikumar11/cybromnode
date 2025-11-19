b1e2b9a7bad7f8ec257a982d534e2a921cb18c8e
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-11-19 10:46:07 +0530

    initial commit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from "axios";
 const Home = () => {
+  const[image,setImage]=useState(null);
+  const handleInput=(e)=>{
+      setImage(e.target.files[0]);
+  }
+  const handleSubmit=async()=>{
+        const frmdata=new FormData();
+        frmdata.append("myfile",image);
+        const res=axios.post(`${import.meta.env.VITE_SERVER}/imageupload`,frmdata);
+  }
+  console.log(image);
+
   return (
     <div>
       <h1>Welcome to home page</h1>
+      <h2>Upload File</h2>
+      File:<input type="file" onChange={handleInput}/>
+      <button onClick={handleSubmit}>Upload</button>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const stuRoute=require("./routes/StudentRoute");
 const errorHandler = require("./middlewares/erroHandler");
+const multer = require("multer");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -15,7 +16,18 @@ app.use("/student",stuRoute);
 
 mongoose.connect(process.env.DBCONN).then(() => console.log("connected to db"));
 
-
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"uploads/");
+    },
+    filename:(req,file,cb)=>{
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+})
+const upload = multer({ storage: storage });
+app.post("/imageupload",upload.single("myfile"),(req,res)=>{
+    res.send("file uploaded");
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
